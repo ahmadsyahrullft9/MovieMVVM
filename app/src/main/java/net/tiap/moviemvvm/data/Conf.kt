20d9fd248170b131abee730e91e753c54986f9a2
ahmadsyahrullft9 <ahmadsyahrullft9@gmail.com>
2020-06-02 15:57:55 +0700

    push api key themoviedb

--- app/src/main/java/net/tiap/moviemvvm/data/Conf.kt
+++ app/src/main/java/net/tiap/moviemvvm/data/Conf.kt
@@ -1,7 +1,7 @@
 package net.tiap.moviemvvm.data
 
 object Conf {
-    val API_KEY = "86e62748523af4bc5706caab37bed6f7"
+    val API_KEY = "push your api key here"
     val BASE_URL = "https://api.themoviedb.org/3/"
     val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w500/"
     val FIRST_PAGE = 1
